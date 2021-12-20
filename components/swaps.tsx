a68306811d5686984fc11043f3b3a1ed647beb6f
kristjangrm <kristjan.grm1@gmail.com>
2021-12-20 18:49:37 +0100

    configured tailwind to work with webstorm

---
+++ components/swaps.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Swaps = () => {
+  return (
+    <div>
+      <h1 className="">Swaps</h1>
+    </div>
+  );
+};
+
+export default Swaps;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
