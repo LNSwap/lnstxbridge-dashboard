e649ae0553709005724ce4ef75066bebb5c7bfd5
grmkris <kristjan.grm1@gmail.com>
2021-12-26 14:25:27 +0100

    better display of status message

--- components/card.tsx
+++ components/card.tsx
@@ -8,11 +8,11 @@ const Card = (props : any) => {
   }, [])
 
   return (
-    <a href="#"
-       className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <div
+       className="block p-6 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
       <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{props.name}</h5>
-      <p className="font-normal text-gray-700 dark:text-gray-400">{props.status}</p>
-    </a>
+      <p className="font-normal text-gray-700 dark:text-gray-400 break-words">{props.status}</p>
+    </div>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -112,10 +112,12 @@ const Home: NextPage = (props: any) => {
           <div id="main-content" className="h-full w-full bg-gray-50 relative overflow-y-auto lg:ml-64">
             <main>
               <div className="pt-6 px-4">
-                <div className="w-full grid grid-cols-3 xl:grid-cols-3 2xl:grid-cols-3 gap-4">
+                <div className="w-full grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-2 gap-4">
                   <Card status={props.status} name="Status"/>
-                  <Card status={props.lndWalletBalance} name="LND Balance"/>
-                  <Card status={props.rbtcWalletBalance} name="RBTC Balance"/>
+                  <div className="w-full grid grid-cols-2 gap-4">
+                    <Card status={props.lndWalletBalance} name="LND Balance"/>
+                    <Card status={props.rbtcWalletBalance} name="RBTC Balance"/>
+                  </div>
                 </div>
               </div>
               <div className="pt-6 px-4">
