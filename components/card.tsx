736b6a59c93dcc737097ce8faa79fd626ec68ad6
pseudozach <git@pseudozach.com>
2022-02-25 02:51:02 +0100

    fix build error

--- components/card.tsx
+++ components/card.tsx
@@ -16,7 +16,7 @@ const Card = (props: any) => {
         <a
           className="inline-flex items-center h-8 px-4 text-sm text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800"
           href={"https://explorer.stacks.co/address/" + props.address}
-          target="_blank">Explorer</a>
+          target="_blank" rel="noreferrer">Explorer</a>
       }
     </div>
   );
