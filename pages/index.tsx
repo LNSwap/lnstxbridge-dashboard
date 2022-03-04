b7473830f66d22037325ea5672a87e8205ced0f6
pseudozach <zozsoy@gmail.com>
2022-03-04 23:37:24 +0000

    UI improvements

--- pages/index.tsx
+++ pages/index.tsx
@@ -80,6 +80,10 @@ const Home: NextPage = () => {
     return (
       <div className="m-auto mt-40 max-w-2xl">
           <div className="mb-6">
+            <img src="./dashboard.png"  style={{display: 'block', margin: 'auto',}}/>
+            <h1 style={{textAlign: 'center', fontSize: 'x-large',}}>LN-STX Bridge Admin Dashboard</h1>
+          </div>
+          <div className="mb-6">
             <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
               Username</label>
             <input type="email" id="email"
@@ -93,6 +97,7 @@ const Home: NextPage = () => {
             <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
               Password</label>
             <input type="password" id="password"
+                    placeholder="password"
                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                    required onChange={event => {setPassword(event.target.value)}}/>
           </div>
@@ -271,7 +276,7 @@ const Home: NextPage = () => {
 
                   <div className="w-full grid grid-cols-3 gap-4">
                     <Card status={dashboardData.lndWalletBalance} name="Lightning"/>
-                    <Card status={dashboardData.lndOnchainBalance} name="Lightning onchain"/>
+                    <Card status={dashboardData.lndOnchainBalance} name="Onchain"/>
                     {
                       dashboardData.stacksWalletBalance ?
                         dashboardData.stacksWalletBalance.map(item => {
