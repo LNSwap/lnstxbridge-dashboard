f8b1159c00e073ca69214e67899913daec092ccc
pseudozach <git@pseudozach.com>
2023-10-05 06:51:37 +0000

    add refund button to UI add trigger refund function

--- components/reverseSwaps.tsx
+++ components/reverseSwaps.tsx
@@ -28,6 +28,7 @@ export interface ReverseSwapProps {
   transactionVout: number;
   createdAt: Date;
   updatedAt: Date;
+  refundable?: boolean;
 }
 
 const ReverseSwaps = (props: any) => {
@@ -93,6 +94,13 @@ const ReverseSwaps = (props: any) => {
       sortable: true,
     },
     {
+      name: 'Refundable',
+      cell: (row: ReverseSwapProps) => row.refundable ? <button className="inline-flex items-center h-8 px-4 m-2 text-sm text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800"  onClick={async () => triggerRefund(row)}>Refund</button> : null,
+      maxWidth: '500px',
+      minWidth: '200px',
+      sortable: true,
+    },
+    {
       name: 'Failure Reason',
       selector: (row: ReverseSwapProps) => row.failureReason,
       maxWidth: '150px',
@@ -138,6 +146,12 @@ const ReverseSwaps = (props: any) => {
     }
   ];
 
+  const triggerRefund = async (swap: ReverseSwapProps) => {
+    let beforeHeaders = {headers: {'Authorization' : props.auth, 'Content-Type': 'application/json'}, method: 'POST', body: JSON.stringify({id: swap.id})};
+    let refundType = 'stacks';
+    const response: any = await fetch(`${props.apiurl}/api/admin/${refundType}/refund`, beforeHeaders).then(res => res.json());
+    console.log('reverse triggerRefund ', response);
+  }
 
   return (
   <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8  2xl:col-span-2">
